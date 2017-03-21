01d83c0b4931085a770d79b300bf957fe3b8f106
chenchen02 <2899208030@qq.com>
2017-03-21 18:19:09 +0800

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,21 @@
+
+    $(document).ready(function(){
+        $('#button').click(function(){
+            $.ajax({
+                type:"GET",
+                url:"D:/mddemo/ajax/json/music.txt", //路径
+                dataType:"json",
+                success:function(data){
+                var music="<h2>";
+                //i表示在data中的索引位置，n表示包含的信息的对象
+                $.each(data,function(i,n){
+                    //获取对象中属性为optionsValue的值
+                    music+="<div>"+n["optionValue"]+"</div>";
+                });
+                music+="</h2>";
+                    $('#result').append(music);
+                }
+            });
+            return false;
+        });
+    });
