a37083ab3b27aeb4c1372ce1b8774a6bed700f34
chenchen02 <2899208030@qq.com>
2017-03-22 11:23:22 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -1,16 +1,7 @@
 ﻿
     $(document).ready(function(){
         $('.input3').click(function(){
-            $.ajax({
-                type:"GET",
-                url:"music.txt", //路径
-                dataType:"json",
-                success:function(data){
-
-              $('.input1').val()=data.user;
-
-                };
-            });
+          alert('aa');
            
         });
     });
