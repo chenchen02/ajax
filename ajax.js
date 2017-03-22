7d6ef1e9470b284fc9e9e554445ea6ce60042e2c
chenchen02 <2899208030@qq.com>
2017-03-22 11:01:13 +0800

    更

--- ajax.js
+++ ajax.js
@@ -1,20 +1,14 @@
-
+﻿
     $(document).ready(function(){
-        $('#button').click(function(){
+        $('.input3').click(function(){
             $.ajax({
                 type:"GET",
                 url:"music.txt", //路径
                 dataType:"json",
                 success:function(data){
-                var music="<h2>";
-                //i表示在data中的索引位置，n表示包含的信息的对象
-                $.each(data,function(i,n){
-                    //获取对象中属性为optionsValue的值
-                    music+="<div>"+n["optionValue"]+"</div>";
-                });
-                music+="</h2>";
-                    $('#result').append(music);
-                }
+
+alert($('.input3').val()==data.user)
+                };
             });
             return false;
         });
