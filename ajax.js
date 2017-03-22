07f9856d0a904af9335f378772f2f81e4e18dc9f
chenchen02 <2899208030@qq.com>
2017-03-22 13:01:46 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -1,8 +1,16 @@
 ﻿
     $(document).ready(function(){
         $(".input3").click(function(){
-         $(".input1").val("12");
-   window.open("http://www.baidu.com");
+
+$.ajax({
+                type:"GET",
+                url:"music.txt", //路径
+                dataType:"json",
+                success:function(data){
+
+         $(".input1").val(date);
+   windw.open("www.baidu.com");
+}
            
         });
-    });
+    })
