43868cf501dde965062e0991a41ad50e2decfbeb
chenchen02 <2899208030@qq.com>
2017-03-22 14:43:56 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -2,18 +2,16 @@
     $(document).ready(function(){
         $(".input3").click(function(){
 
-$.ajax({
-
-url:"music.txt",data: "json",async:false,
-
-success: function(resultData){
-         $.each(resultData,function(i,item){
-        alert(item.firstName);
-    })
-        }
-
-});
-  
+$.ajax({       
+    type:"POST",//请求方式  
+    url:music.txt,//请求路径  
+    dataType: "json",//数据格式  
+    success: function(resultData){//成功处理函数        
+    $.each(resultData,function(i,item){//遍历返回json数据，i为数据号，item当前数据  
+        alert(item.firstName);//弹出每一个project  
+    })//多多注意括号，没有;结束符  
+    }  
+})  
 
            
         });
