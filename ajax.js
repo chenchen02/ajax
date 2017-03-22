8112d3c0b68a605bce1cd08704a258317f8106c1
chenchen02 <2899208030@qq.com>
2017-03-22 14:07:52 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -2,9 +2,9 @@
     $(document).ready(function(){
         $(".input3").click(function(){
 
-$.get("music.txt", function(data){
-  alert("Data Loaded: " + data);
-});
+
+  alert("Data Loaded: " );
+
            
         });
     });
