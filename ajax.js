b6eddbbfffa976d6726cb366a86363c84a587da0
chenchen02 <2899208030@qq.com>
2017-03-22 14:51:12 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -6,10 +6,10 @@ $.ajax({
     type:"POST",//请求方式  
     url:"music.txt",//请求路径
     dataType: "json",//数据格式  
-    success: function(resultData){//成功处理函数        
-    $.each(resultData,function(i,item){//遍历返回json数据，i为数据号，item当前数据  
-        alert(item.firstName);//弹出每一个project  
-    })//多多注意括号，没有;结束符  
+    success: function(cvvd){//成功处理函数
+    //遍历返回json数据，i为数据号，item当前数据
+        alert(ccvd);//弹出每一个project
+    //多多注意括号，没有;结束符
     } ,
     error:function(){
         alert("error");
