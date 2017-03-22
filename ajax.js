e0a84409da2b54ea0c19b39db6bcd43ddf281a71
chenchen02 <2899208030@qq.com>
2017-03-22 12:11:05 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -2,7 +2,7 @@
     $(document).ready(function(){
         $(".input3").click(function(){
          $(".input1").val("12");
-   windw.open(' http://www.baidu.com','_blank');
+   windw.open("http://www.baidu.com");
            
         });
     });
