a13752c5d4e3683e53e55ec405803fcb0af977f1
chenchen02 <2899208030@qq.com>
2017-03-22 15:18:45 +0800

    jaj

--- ajax.js
+++ ajax.js
@@ -2,19 +2,9 @@
     $(document).ready(function(){
         $(".input3").click(function(){
 
-$.ajax({  
-     
-    type:"POST",//请求方式  
-    url:"music.txt",//请求路径
-    dataType: "json",//数据格式  
-    success: function(){
-   
-        alert("success");
-    } ,
-    error:function(){
-        alert("error");
-    }
-})  
+            $.get("music.txt", function(result){
+                alert("success");
+            });
 
            
         });
